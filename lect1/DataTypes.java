ac9b92c60aa1ff69ab04e288e02272cec4ca599f
sakshi khare <sakshikhare2006@gmail.com>
2025-11-12 13:48:15 +0530

    hello world

---
+++ lect1/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+        public static void main(String[] args){
+            short b=5;
+            int c=5;
+            float f= 3.14f;
+            boolean data = false;
+            char Hashtag='#';
+            char star='*';
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(f);
+            System.out.println(data);
+            System.out.println(Hashtag);
+            System.out.println(star);
+        }
+    }
+
