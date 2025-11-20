961de3959b111c03acc69e1ae9557a2804d81daf
sakshi khare <sakshikhare2006@gmail.com>
2025-11-20 14:47:36 +0530

    string

---
+++ lect5/VowelCount.java
@@ -0,0 +1,15 @@
+// count vowel in string
+public class VowelCount {
+    public static void main(String[] args){
+        String str = "hello is powerful";
+        int count = 0;
+        for (char ch : str.toCharArray()) {
+            if ("aeiou".indexOf(ch) != -1) {
+                count++;
+            }
+        }
+
+        System.out.println(count); // Output: 6
+    }
+}
+
